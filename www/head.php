006d70311d4e963c03137f67ce4771a2f2d0b142
takya0909 <takuya19940909@yahoo.co.jp>
2020-11-28 16:15:23 +0900

    headerパーツ

---
+++ www/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    .test{
+      display: flex;
+      text-align: center;
+    }
+    .test div{
+      padding: 5px;
+    }
+  </style>
+</head>
+<body>
