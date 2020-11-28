8d99120f314abc0b05480284cb4f0611ee7f9552
takya0909 <takuya19940909@yahoo.co.jp>
2020-11-28 13:34:31 +0900

    TestDetaの追加

--- data/init.sql
+++ data/init.sql
@@ -3,4 +3,34 @@ CREATE DATABASE fovo;
 USE fovo;
 DROP TABLE IF EXISTS name;
 
+CREATE TABLE user (
+  id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(32) NOT NULL,
+)DEFAULT CHARACTER SET=utf8;
 
+CREATE TABLE post (
+		id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+		user_id INT(11) NOT NULL,
+		name VARCHAR(32) NOT NULL,
+		post VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+)DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE favo(
+		id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+		user_id INT(11) NOT NULL,
+    post_id INT(11) NOT NULL,
+    del_flg INT(11) NOT NULL,
+    PRIMARY KEY(id)
+)DEFAULT CHARACTER SET=utf8;
+
+insert into user(name) values('takuya');
+insert into user(name) values('kato');
+insert into user(name) values('ito');
+
+insert into post(user_id,name,post) values(1,'takuya','test1');
+insert into post(user_id,name,post) values(2,'kato','test2');
+insert into post(user_id,name,post) values(3,'ito','test3');
+
+insert into fovo(user_id,post_id) values(1,2);
+insert into fovo(user_id,post_id) values(1,3);
